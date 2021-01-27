4ec0f47527043fab5f49ec605512ee832ef843d4
Michael <Michaelfigueroa24@gmail.com>
2021-01-27 11:25:25 -0600

    worked on c++

---
+++ Days/Day_3/C++/main.cpp
@@ -0,0 +1,20 @@
+// Demo Program | Bookstore Entering Two Numbers and Adding them
+
+#include <iostream>
+
+
+
+int main()
+{
+
+    std::cout << "Enter two numbers: " << std::endl;
+    int v1 = 0, v2 = 0;
+
+    std::cin >> v1 >> v2;
+
+    std::cout << "The sum is " << v1 + v2 << std::endl;
+
+
+
+    return 0;
+}
