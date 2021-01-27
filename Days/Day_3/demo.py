23272d107daebb6afe43f306ec4126c6c2d86d53
Michael <Michaelfigueroa24@gmail.com>
2021-01-27 11:04:23 -0600

    Day 3 | Update

---
+++ Days/Day_3/demo.py
@@ -0,0 +1,18 @@
+# This program asks a user for his name
+
+# This is a comment
+
+
+
+print("Hello World!")
+print("What is your name?")
+
+name = input()
+#print("Nice to meet you " + name + " !") # print to terminal
+#print(len(name)) # counts and prints lenght of char in name
+
+# Examples using data conversions
+age = input("How old are you?")
+
+print("Next year you will be " + str(int(age) + 1) + " years old")
+
